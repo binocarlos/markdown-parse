823b1b6d63dc84aab63831ce07ce4348c0d46502
Kai Davenport <kaiyadavenport@gmail.com>
2014-06-28 15:01:15 +0000

    stream interface

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var fm = require('front-matter');
 var marked = require('marked');
+var concat = require('concat-stream')
+var through = require('through2')
+var duplexer = require('duplexer')
 
 marked.setOptions({
   gfm: true,
@@ -12,14 +15,32 @@ marked.setOptions({
   langPrefix: ''
 })
 
-module.exports = function(string, done){
-	var frontmatter = fm(string);
-	var matterdata = frontmatter.attributes;
+function parse(string, done){
+  var frontmatter = fm(string);
+  var matterdata = frontmatter.attributes;
 
-	marked(frontmatter.body, function (err, html){
-		if(err) return done(err)
+  marked(frontmatter.body, function (err, html){
+    if(err) return done(err)
 
-		frontmatter.html = html
-		done(null, frontmatter)
-	})	
+    frontmatter.html = html
+    done(null, frontmatter)
+  })  
+}
+
+module.exports = parse
+
+module.exports.stream = function(){
+
+  var output = through()
+  var input = concat(function(input){
+    parse(input.toString(), function(err, result){
+      if(err){
+        console.error(err)
+        process.exit(1)
+      }
+      output.end(JSON.stringify(result))
+    })
+  })
+
+  return duplexer(input, output)
 }
